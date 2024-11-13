eb55b56c03c03d5f92a8296897ef1e035397b5ae
Oscar Blais <oscar.blais@etudiant.univ-lr.fr>
2024-11-13 17:16:28 +0100

    wip: test CI 22

--- tests/test_function.py
+++ tests/test_function.py
@@ -12,39 +12,3 @@ class TestFunctions(TestCase):
 
     def test_invalid_pgcd(self):
         self.assertNotEqual(0, pgcd(40902, 24140))
-
-    def test_valid_pgcd2(self):
-        self.assertEqual(34, pgcd(40902, 24140))
-
-    def test_invalid_pgcd2(self):
-        self.assertNotEqual(0, pgcd(40902, 24140))
-
-    def test_valid_pgcd3(self):
-        self.assertEqual(34, pgcd(40902, 24140))
-
-    def test_invalid_pgcd3(self):
-        self.assertNotEqual(0, pgcd(40902, 24140))
-
-    def test_valid_pgcd4(self):
-        self.assertEqual(34, pgcd(40902, 24140))
-
-    def test_invalid_pgcd4(self):
-        self.assertNotEqual(0, pgcd(40902, 24140))
-
-    def test_valid_pgcd5(self):
-        self.assertEqual(34, pgcd(40902, 24140))
-
-    def test_invalid_pgcd5(self):
-        self.assertNotEqual(0, pgcd(40902, 24140))
-
-    def test_valid_pgcd6(self):
-        self.assertEqual(34, pgcd(40902, 24140))
-
-    def test_invalid_pgcd6(self):
-        self.assertNotEqual(0, pgcd(40902, 24140))
-
-    def test_valid_pgcd7(self):
-        self.assertEqual(34, pgcd(40902, 24140))
-
-    def test_error(self):
-        self.assertEqual(1, 2)
